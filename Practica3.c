2e7b455ebd9c9c3849f4ccf6072bc47a8e2fc328
groteck <groteck@gmail.com>
2012-03-14 09:52:18 +0000

    need for writing data in *.txt

---
+++ Practica3.c
@@ -0,0 +1,85 @@
+#include "ami.h"
+#include "ami_bmp.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#include <string.h>
+
+void LotkaVolterra(float a, float b, float d, float e, float f, float P0,
+float C0, float  *Presa, float *Cazador, float dt, int NIter){
+    float Pant = P0, Pact, Cant = C0, Cact;
+    int i=0;
+
+    Presa[0] = P0;
+    Cazador[0] = C0;
+    Pact = Pant +(dt*((a-(b*Pant)-(d*Cant))*Pant));
+    if(Pact < 0.5){
+       Pact=0;
+    };
+    Cact = Cant +(dt*((-e+(f*Pant))*Cant));
+    if(Cact < 0.5){
+        Cact=0;
+    };
+    Pant = Pact;
+    Cant = Cact;
+
+    while((i<NIter)&& (Pact > 0.5) && (Cact > 0.5)){
+
+        Pact = Pant +(dt*((a-(b*Pant)-(d*Cant))*Pant));
+        if(Pact < 0.5){
+                Pact=0;
+        };
+        Cact = Cant +(dt*((-e+(f*Pant))*Cant));
+        if(Cact < 0.5){
+                Cact=0;
+        };
+        Pant = Pact;
+        Cant = Cact;
+        i++;
+	Presa[i] = Pact;
+	Cazador[i] = Cact;
+    }
+};
+
+int main() {
+	int NIter1 = 2000;
+	float a1, b1, d1, e1, f1, P01, C01, Presa1[NIter1], Cazador1[NIter1], dt1;
+	int NIter2 = 2000;
+	float a2, b2, d2, e2, f2, P02, C02, Presa2[NIter2], Cazador2[NIter2], dt2;
+	int NIter3 = 2000;
+	float a3, b3, d3, e3, f3, P03, C03, Presa3[NIter3], Cazador3[NIter3], dt3;
+	int NIter4 = 2000;
+	float a4, b4, d4, e4, f4, P04, C04, Presa4[NIter4], Cazador4[NIter4], dt4;
+ 
+	LotkaVolterra (a1, b1, d1, e1, f1, P01, C01, Presa1, Cazador1, dt1, NIter1);
+	LotkaVolterra (a2, b2, d2, e2, f2, P02, C02, Presa2, Cazador2, dt2, NIter2);
+	LotkaVolterra (a3, b3, d3, e3, f3, P03, C03, Presa3, Cazador3, dt3, NIter3);
+	LotkaVolterra (a4, b4, d4, e4, f4, P04, C04, Presa4, Cazador4, dt4, NIter4);
+
+	FILE *fp1;
+	char cadena1[] = "%s, %s \n", Presa1, Cazador1;
+        fp1 = fopen ( "equilibrio.txt", "r+" );
+        fputs( cadena1, fp1 );
+        fclose ( fp1 );
+
+	FILE *fp2;
+	char cadena2[] = "%s, %s \n", Presa2, Cazador2;
+        fp2 = fopen ( "depre-muertos.txt", "r+" );
+        fputs( cadena2, fp2 );
+        fclose ( fp2 );
+
+	FILE *fp3;
+	char cadena3[] = "%s, %s \n", Presa3, Cazador3;
+        fp3 = fopen ( "muertos.txt", "r+" );
+        fputs( cadena3, fp3 );
+        fclose ( fp3 );
+
+	FILE *fp4;
+	char cadena4[] = "%s, %s \n", Presa4, Cazador4;
+        fp4 = fopen ( "grndes.txt", "r+" );
+        fputs( cadena4, fp4 );
+        fclose ( fp4 );
+        return 0;
+}
+
+
